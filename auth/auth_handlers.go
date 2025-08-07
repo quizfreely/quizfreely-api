f45bc8a79cb4e7bfd7366b720d07735a1bcf5298
Ehan <ehanahamed@ehan.dev>
2025-08-07 02:04:47 -0400

    work in progress

--- auth/auth_handlers.go
+++ auth/auth_handlers.go
@@ -4,6 +4,17 @@ import (
 	"net/http"
 )
 
+type SignUpReqBody struct {
+	Username string `json:"username"`
+	NewPassword string `json:"password"`
+}
+
 func SignUpHandler(w http.ResponseWriter, r *http.Request) {
-	
+	var reqBody SignUpReqBody
+	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
+		http.Error(w, "Invalid request body", 400)
+		return
+	}
+	w.WriteHeader(201)
+	w.W
 }
