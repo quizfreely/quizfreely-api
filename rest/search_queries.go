f6e67e1fc808f225d9049dfa5285f1b8f83f99bc
Ehan <ehanahamed@ehan.dev>
2025-08-09 21:40:57 -0400

    fix broken search queries rest implementation idk what was going on before this

--- rest/search_queries.go
+++ rest/search_queries.go
@@ -2,7 +2,9 @@ package rest
 
 import (
 	"net/http"
+	"regexp"
 	"strconv"
+	"strings"
 	"quizfreely/api/graph/model"
 
 	"github.com/georgysavva/scany/v2/pgxscan"
@@ -10,16 +12,69 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+// async function searchQueries(query, limit, offset) {
+//     try {
+//         /*
+//             replace whitespace (tabs, spaces, etc and multiple) with a space
+//             whitespace characters next to eachother will be replaced with a single space
+//         */
+//         let spaceRegex = /\s+/gu;
+//         let inputQuery = query.replaceAll(spaceRegex, " ");
+//         /* after that, replace and sign ("&") with "and" */
+//         inputQuery = inputQuery.replaceAll("&", "and");
+//         /*
+//             after "sanitizing" spaces, remove special characters
+//             this will keep letters (any alphabet) accent marks, numbers (any alphabet), underscore, period/dot, and dashes
+//         */
+//         let rmRegex = /[^\p{L}\p{M}\p{N} _.-]/gu;
+//         inputQuery = inputQuery.replaceAll(rmRegex, "");
+//         let result;
+//         if (inputQuery.length <= 3) {
+//             result = await pool.query(
+//                 "select query, subject from public.search_queries " +
+//                 "where query ilike $1 limit $2 offset $3",
+//                 [
+//                     /* percent sign (%) to match querys that start with inputQuery */
+//                     (inputQuery + "%"),
+//                     limit,
+//                     offset
+//                 ]
+//             )
+//         } else {
+//             result = await pool.query(
+//                 "select query, subject from public.search_queries " +
+//                 "where similarity(query, $1) > 0.15 " +
+//                 "order by similarity(query, $1) desc limit $2 offset $3",
+//                 [
+//                     inputQuery,
+//                     limit,
+//                     offset
+//                 ]
+//             )
+//         }
+//         return {
+//             data: result.rows
+//         }
+//     } catch (error) {
+//         return {
+//             error: error
+//         }
+//     }
+// }
+
+var spaceRegex = regexp.MustCompile(`\s+`)
+var removeRegex = regexp.MustCompile(`[^\pL\pM\pN _.\-]`)
+
 func (rh *RESTHandler) GetSearchQueries(w http.ResponseWriter, r *http.Request) {
 	searchParams := r.URL.Query() /* query string */
-	q := searchParams.Get("q")
+	rawQuery := searchParams.Get("q")
 	limit := searchParams.Get("limit")
 	offset := searchParams.Get("offset")
 
 	l := 5
 	if limit != "" {
 		lInt, err := strconv.Atoi(limit)
-		if err == nil && lInt > 0 && lInt < 5 {
+		if err == nil && lInt > 0 {
 			l = lInt
 		}
 	}
@@ -32,26 +87,31 @@ func (rh *RESTHandler) GetSearchQueries(w http.ResponseWriter, r *http.Request) 
 		}
 	}
 
+	// Sanitize input query
+	inputQuery := spaceRegex.ReplaceAllString(rawQuery, " ")
+	inputQuery = strings.ReplaceAll(inputQuery, "&", "and")
+	inputQuery = removeRegex.ReplaceAllString(inputQuery, "")
+	log.Info().Msg(inputQuery)
+
 	var searchQueries []*model.SearchQuery
-	sql := `
-		SELECT
-			id,
-			query,
-			to_char(updated_at, 'YYYY-MM-DD"T"HH24:MI:SS.MSTZH:TZM') as updated_at,
-			(SELECT display_name FROM auth.users WHERE id = search_queries.user_id) AS user_display_name,
-			(SELECT COUNT(*) FROM public.studysets WHERE document @@ websearch_to_tsquery('english', search_queries.query)) AS studyset_count
-		FROM public.search_queries
-		WHERE query ILIKE $1
-		ORDER BY (SELECT COUNT(*) FROM public.studysets WHERE document @@ websearch_to_tsquery('english', search_queries.query)) DESC
-		LIMIT $2 OFFSET $3
-	`
-	err := pgxscan.Select(r.Context(), rh.DB, &searchQueries, sql, "%"+q+"%", l, o)
+	var err error
+
+	if len(inputQuery) <= 3 {
+		err = pgxscan.Select(r.Context(), rh.DB, &searchQueries,
+			"SELECT query, subject FROM public.search_queries WHERE query ILIKE $1 ORDER BY query LIMIT $2 OFFSET $3",
+			inputQuery+"%", l, o)
+	} else {
+		err = pgxscan.Select(r.Context(), rh.DB, &searchQueries,
+			"SELECT query, subject FROM public.search_queries WHERE similarity(query, $1) > 0.15 ORDER BY similarity(query, $1) DESC LIMIT $2 OFFSET $3",
+			inputQuery, l, o)
+	}
+
 	if err != nil {
-		log.Error().Err(err).Msg("Database err in GetSearchQueries")
+		log.Error().Err(err).Msg("Database error in GetSearchQueries")
 		render.Status(r, 500)
 		render.JSON(w, r, map[string]interface{}{
 			"error": map[string]interface{}{
-				"statusCode": 400,
+				"statusCode": 500,
 				"message":    "Database error in GetSearchQueries",
 			},
 		})
@@ -59,7 +119,7 @@ func (rh *RESTHandler) GetSearchQueries(w http.ResponseWriter, r *http.Request) 
 	}
 
 	render.JSON(w, r, map[string]interface{}{
-		"error": false,
+		"error": nil,
 		"data": map[string]interface{}{
 			"queries": searchQueries,
 		},
