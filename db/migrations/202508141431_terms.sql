6e45ef84395e6f53db3d8b1c1f3f07451d83daae
Ehan <ehanahamed@ehan.dev>
2025-08-14 15:53:14 -0400

    finish making new tables

--- db/migrations/202508141431_terms.sql
+++ db/migrations/202508141431_terms.sql
@@ -3,7 +3,9 @@ create table terms (
     id uuid primary key default gen_random_uuid(),
     term text,
     def text,
-    studyset_id uuid not null references studysets (id) on delete cascade
+    studyset_id uuid not null references studysets (id) on delete cascade,
+    created_at timestamptz default now(),
+    updated_at timestamptz default now()
 );
 
 create table term_progress (
@@ -16,7 +18,23 @@ create table term_progress (
     def_first_reviewed_at timestamptz,
     def_last_reviewed_at timestamptz,
     def_review_count int,
-    term_leitner_box smallint,
-    def_leitner_box smallint
+    term_leitner_system_box smallint,
+    def_leitner_system_box smallint
 );
+
+INSERT INTO terms (term, def, studyset_id)
+SELECT
+    elem->>0 AS term,
+    elem->>1 AS def,
+    s.id AS studyset_id
+FROM studysets s
+CROSS JOIN LATERAL jsonb_array_elements(s.data->'terms') AS elem;
+
+ALTER TABLE studysets
+DROP COLUMN data;
+
+DROP TABLE studyset_progress;
+
+DROP TABLE studyset_settings;
+
 -- migrate:down
